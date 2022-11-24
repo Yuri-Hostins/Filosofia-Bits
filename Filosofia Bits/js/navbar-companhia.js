5df3cdbaa5d696e5366ccd04cf090713180dc661
Yuri <107286916+Yuri-Hostins@users.noreply.github.com>
2022-11-24 16:28:26 -0300

    Delete Filosofia Bits directory

--- Filosofia Bits/js/navbar-companhia.js
+++
@@ -1,9 +0,0 @@
-function openSlideMenu () {
-    document.getElementById('menu').style.width = '100%';
-    document.getElementById('content').style.marginLeft = '100%';
-  }
-  
-  function closeSlideMenu () {
-    document.getElementById('menu').style.width = '0';
-    document.getElementById('content').style.marginLeft = '0';
-  }
