f513d2bf76c5b178784074cc399e99a6ba58cdd6
Yuri <107286916+Yuri-Hostins@users.noreply.github.com>
2023-01-23 00:36:03 -0300

    Add files via upload

---
+++ Filosofia Bits/js/navbar-companhia.js
@@ -0,0 +1,9 @@
+function openSlideMenu () {
+    document.getElementById('menu').style.width = '100%';
+    document.getElementById('content').style.marginLeft = '100%';
+  }
+  
+  function closeSlideMenu () {
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('content').style.marginLeft = '0';
+  }
