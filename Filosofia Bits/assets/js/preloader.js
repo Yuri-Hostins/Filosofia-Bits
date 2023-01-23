296fc405d6c9ba3058d0975208767971c2d2d089
Yuri <107286916+Yuri-Hostins@users.noreply.github.com>
2023-01-23 18:16:33 -0300

    Add files via upload

---
+++ Filosofia Bits/assets/js/preloader.js
@@ -0,0 +1,8 @@
+//<![CDATA[
+  $(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(10).fadeOut('slow'); 
+    $('body').delay(10).css({'overflow': 'visible'});
+  })
+  //]]>
+  
